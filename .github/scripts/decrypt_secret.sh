a482a070e66f5ec24c6ae3f2f2bcf96f88dca58b
j.lu1072298 <50785101+twatziki@users.noreply.github.com>
2020-06-27 18:10:58 -0400

    Update decryption shell script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSWORD" \
 --output ./app/google-services.json ./app/google-services.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSWORD" \
+--output ./app/src/main/res/values/secrets.xml ./app/secrets.xml.gpg
